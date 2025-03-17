de8f3a2919851354edf9f3ef15b10eea5eb08040
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-17 23:03:43 +0100

    build: Add local version catalog: local.versions.toml
    This will allow us to define versions, libraries, bundles and plugins in our build.gradle.kts files which will be useful for a multi-module Gradle project. Update root build.gradle.kts to use Gradle dependencies from local.versions.toml.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.4.3"
-	id("io.spring.dependency-management") version "1.1.7"
+	alias(local.plugins.springboot)
+	alias(local.plugins.spring.dependencies)
 }
 
 group = "com.github.thorlauridsen"
@@ -18,9 +18,25 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	// Spring Boot dependencies
+	implementation(local.springboot.starter)
+	implementation(local.springboot.starter.jpa)
+	implementation(local.springboot.starter.web)
+
+	// H2 database dependency for in-memory database
+	implementation(local.h2database)
+
+	// Liquibase core dependency for database migrations
+	implementation(local.liquibase.core)
+
+	// Springdoc OpenAPI for providing Swagger documentation
+	implementation(local.springdoc.openapi.starter.webmvc)
+
+	// Spring Boot test dependencies
+	testImplementation(local.springboot.starter.test)
+
+	// JUnit platform launcher dependency for running JUnit tests
+	testRuntimeOnly(local.junit.platform.launcher)
 }
 
 tasks.withType<Test> {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
 rootProject.name = "sample"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("local") {
+            from(files("gradle/local.versions.toml"))
+        }
+    }
+}
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+    }
+}
