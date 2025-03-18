ae2ce4e9cbcd6b63c85b08c34a0bf6bf0b77c836
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-18 14:19:37 +0100

    docs: Add code documentation to SampleApplication

--- apps/api/src/main/java/com/github/thorlauridsen/SampleApplication.java
+++ apps/api/src/main/java/com/github/thorlauridsen/SampleApplication.java
@@ -3,6 +3,9 @@ package com.github.thorlauridsen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry point for the application.
+ */
 @SpringBootApplication
 public class SampleApplication {
 
