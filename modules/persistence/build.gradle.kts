f916b907ca93dee02f1b9f375f739f183110e81b
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-18 11:18:22 +0100

    build: Remove unused Gradle dependencies in persistence build.gradle.kts

--- modules/persistence/build.gradle.kts
+++ modules/persistence/build.gradle.kts
@@ -10,20 +10,4 @@ dependencies {
     // Spring Boot dependencies
     implementation(local.springboot.starter)
     implementation(local.springboot.starter.jpa)
-
-    // H2 database dependency for in-memory database
-    implementation(local.h2database)
-
-    // Liquibase core dependency for database migrations
-    implementation(local.liquibase.core)
-
-    // Spring Boot test dependencies
-    testImplementation(local.springboot.starter.test)
-
-    // JUnit platform launcher dependency for running JUnit tests
-    testRuntimeOnly(local.junit.platform.launcher)
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
 }
