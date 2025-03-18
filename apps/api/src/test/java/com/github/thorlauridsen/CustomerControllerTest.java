6681256295fa441c9bc75b223fb6723a4a0356eb
Thor Tudal Lauridsen <30320530+thorlauridsen@users.noreply.github.com>
2025-03-18 12:46:20 +0100

    test: Add CustomerControllerTest (#7)
    - Delete `SampleApplicationTests`.
    - Add test class `BaseMockMvc` which provides a base test class for controller tests.
      - Includes function `mockGet()` for exeucting a mock HTTP GET request to a given endpoint.
      - Includes function `mockPost()` which takes a JSON body and executes a mock HTTP POST request to a given endpoint.
    - Add `CustomerControllerTest` which includes tests for creating customers and retrieving customers. These tests will assert the response HTTP status code and verify the contents of the returned customer.

---
+++ apps/api/src/test/java/com/github/thorlauridsen/CustomerControllerTest.java
@@ -0,0 +1,64 @@
+package com.github.thorlauridsen;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.thorlauridsen.dto.CustomerDto;
+import com.github.thorlauridsen.dto.CustomerInputDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.UUID;
+
+import static com.github.thorlauridsen.controller.Endpoint.CUSTOMER_BASE_ENDPOINT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CustomerControllerTest extends BaseMockMvc {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    public CustomerControllerTest(MockMvc mockMvc) {
+        super(mockMvc);
+    }
+
+    @Test
+    public void getCustomer_randomId_returnsNotFound() throws Exception {
+        UUID id = UUID.randomUUID();
+        var response = mockGet(CUSTOMER_BASE_ENDPOINT + "/" + id);
+        assertEquals(HttpStatus.NOT_FOUND.value(), response.getStatus());
+    }
+
+    @Test
+    public void postCustomer_getCustomer_success() throws Exception {
+        CustomerInputDto customer = new CustomerInputDto("bob@gmail.com");
+        String json = objectMapper.writeValueAsString(customer);
+        var response = mockPost(json, CUSTOMER_BASE_ENDPOINT);
+        assertEquals(HttpStatus.CREATED.value(), response.getStatus());
+
+        String responseJson = response.getContentAsString();
+        CustomerDto createdCustomer = objectMapper.readValue(responseJson, CustomerDto.class);
+        assertCustomer(createdCustomer, "bob@gmail.com");
+
+        var response2 = mockGet(CUSTOMER_BASE_ENDPOINT + "/" + createdCustomer.id());
+        assertEquals(HttpStatus.OK.value(), response2.getStatus());
+
+        String responseJson2 = response2.getContentAsString();
+        CustomerDto fetchedCustomer = objectMapper.readValue(responseJson2, CustomerDto.class);
+        assertCustomer(fetchedCustomer, "bob@gmail.com");
+    }
+
+    /**
+     * Ensure that customer is not null and that the id is not null.
+     * Assert that the mail is equal to the expected mail.
+     *
+     * @param customer     {@link CustomerDto}
+     * @param expectedMail Expected mail of the customer.
+     */
+    private void assertCustomer(CustomerDto customer, String expectedMail) {
+        assertNotNull(customer);
+        assertNotNull(customer.id());
+        assertEquals(expectedMail, customer.mail());
+    }
+}
