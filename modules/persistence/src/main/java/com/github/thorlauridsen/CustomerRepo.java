9a43224f2a9be50f0d96efe9dbc53c4ea06d929d
Thor Tudal Lauridsen <Thor.Lauridsen@ufst.dk>
2025-03-19 10:55:09 +0100

    refac: Remove redundant findById() method in CustomerRepo
    We can safely remove the redundant findById() method in CustomerRepo because this method is already provided by default when we extend JpaRepository.

--- modules/persistence/src/main/java/com/github/thorlauridsen/CustomerRepo.java
+++ modules/persistence/src/main/java/com/github/thorlauridsen/CustomerRepo.java
@@ -1,9 +1,7 @@
 package com.github.thorlauridsen;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.lang.NonNull;
 
-import java.util.Optional;
 import java.util.UUID;
 
 /**
@@ -12,12 +10,4 @@ import java.util.UUID;
  * It extends the {@link JpaRepository} interface which allows us to easily define CRUD methods.
  */
 public interface CustomerRepo extends JpaRepository<CustomerEntity, UUID> {
-
-    /**
-     * Find a customer by id.
-     *
-     * @param id UUID of the customer.
-     * @return {@link Optional} of {@link CustomerEntity}.
-     */
-    @NonNull Optional<CustomerEntity> findById(@NonNull UUID id);
 }
