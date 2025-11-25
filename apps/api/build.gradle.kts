5812b69c272f8926849153117768876903195513
Thor Tudal Lauridsen <30320530+thorlauridsen@users.noreply.github.com>
2025-11-25 19:23:40 +0100

    refac: Update Spring Boot from 3.5.8 to 4.0.0 (#93)
    - Update Spring Boot from 3.5.8 to 4.0.0.
    - Update `junit-platform-launcher` from 1.12.2 to 6.0.1.
    - Update Springdoc from 2.8.14 to 3.0.0.
    - Remove dependency `org.liquibase:liquibase-core`.
    - Add dependency `spring-boot-starter-liquibase`.
    - Add dependency `spring-boot-resttestclient`.
    - Add dependency `org.testcontainers:junit-jupiter`.
    - Migrate dependency from `spring-boot-starter-web` to `spring-boot-starter-webmvc`.
    - Remove `TestContainerConfig` and instead set up a `PostgreSQLContainer` in the necessary tests with PostgreSQL 18.
    - Use `JsonMapper` instead of `ObjectMapper` in tests as this is the new recommended way to serialize JSON using Jackson 3+.
    - Rename `BaseMockMvc` to `BaseControllerTest` and start using the new `RestTestClient` from Spring Boot 4+.

--- apps/api/build.gradle.kts
+++ apps/api/build.gradle.kts
@@ -12,16 +12,16 @@ dependencies {
 	// Spring Boot dependencies
 	implementation(local.springboot.starter)
 	implementation(local.springboot.starter.validation)
-	implementation(local.springboot.starter.web)
+	implementation(local.springboot.starter.webmvc)
+
+	// Spring Boot Liquibase dependency for database migrations
+    implementation(local.springboot.starter.liquibase)
 
 	// H2 database dependency for in-memory database
 	runtimeOnly(local.h2database)
 
 	// FasterXML Jackson support for Java 8 date/time
 	implementation(local.jackson.datatype.jsr310)
-
-	// Liquibase core dependency for database migrations
-	runtimeOnly(local.liquibase.core)
 
 	// PostgreSQL database driver
 	runtimeOnly(local.postgres)
@@ -30,8 +30,10 @@ dependencies {
 	implementation(local.springdoc.openapi.starter.webmvc)
 
 	// Spring Boot and Testcontainers test dependencies
+    testImplementation(local.springboot.resttestclient)
 	testImplementation(local.springboot.starter.test)
 	testImplementation(local.springboot.testcontainers)
+    testImplementation(local.testcontainers.junit.jupiter)
 	testImplementation(local.testcontainers.postgresql)
 
 	// JUnit platform launcher dependency for running JUnit tests
